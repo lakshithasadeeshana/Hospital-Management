6a16c48f17f20455508a96926b48c2c98b7ccf8a
lakshitha Saadeeshana <44288136+lakshithasadeeshana@users.noreply.github.com>
2022-04-07 21:59:29 +0530

    Delete Patients.sql

--- Patients.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Patients]
-(
-	[PatientID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Pname] NCHAR(20) NULL, 
-    [Age] INT NULL, 
-    [Gender] NCHAR(10) NULL, 
-    [BG] NCHAR(10) NULL, 
-    [problem] VARCHAR(MAX) NULL
-)
