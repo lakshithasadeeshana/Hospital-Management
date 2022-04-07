4335d16701ea577857554a5f7ada443229b1511c
lakshitha Saadeeshana <44288136+lakshithasadeeshana@users.noreply.github.com>
2022-04-07 21:59:00 +0530

    Delete UserData.sql

--- UserData.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[UserData]
-(
-	[Id] INT NOT NULL IDENTITY, 
-    [Username] NCHAR(20) NULL, 
-    [password] NCHAR(20) NULL, 
-    [Email] NCHAR(30) NULL, 
-    [job] NCHAR(20) NULL 
-)
